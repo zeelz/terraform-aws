1a527bb8c8ba666a1d11a0fe74147896e141f400
Keme <keme.kenneth@gmail.com>
2025-09-25 15:22:24 +0100

    Update pipeline
    Add deploy

---
+++ ssh-deploy-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apk add --no-cache openssh-client 
+
+ssh-keyscan -H "$ZEELZ_EC2_IP" >> /tmp/known_hosts
+
+ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" "$AWS_USER"@"$ZEELZ_EC2_IP" "
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin &&
+docker pull $DOCKER_USER/express-gitlab &&
+docker ps -aq | xargs docker rm -f || true &&
+docker run -d -p 5500:5500 -e PORT=5500 -e DB_USER=zeelz -e DB_HOST=5.tcp.eu.ngrok.io -e DB_NAME=postgres -e DB_PORT=11010 zeelz/express-gitlab
+"
