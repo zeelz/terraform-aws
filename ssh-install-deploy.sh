58c270ad15b89ddba6433b025da52153ac1369d3
Keme <keme.kenneth@gmail.com>
2025-10-08 10:09:14 +0100

    Update pipeline

--- ssh-install-deploy.sh
+++ ssh-install-deploy.sh
@@ -10,7 +10,7 @@ ssh-keyscan -H "$ZEELZ_EC2_IP" >> /tmp/known_hosts
 chmod 400 "$SSH_PRIVATE_KEY"
 
 file "$SSH_PRIVATE_KEY"
-head -n 2 "$SSH_PRIVATE_KEY"
+# head -n 2 "$SSH_PRIVATE_KEY"
 
 # ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" "$AWS_USER"@"$ZEELZ_EC2_IP" "echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin &&
 # docker pull $DOCKER_USER/express-gitlab &&
