273679ef2a3312bb4fcc06f64d88a52b533746b4
Keme <keme.kenneth@gmail.com>
2025-10-08 10:40:06 +0100

    Update pipeline

--- ssh-install-deploy.sh
+++ ssh-install-deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-apk add --no-cache openssh-client 
-
-# echo "ZEELZ_EC2_IP: '$ZEELZ_EC2_IP'"
+apk add --no-cache openssh-client
 
 echo "ssh-keyscan to known_hosts"
 ssh-keyscan -H "$ZEELZ_EC2_IP" >> /tmp/known_hosts
 
 chmod 400 "$SSH_PRIVATE_KEY" # stored as file
 
-file "$SSH_PRIVATE_KEY"
-head -n 2 "$SSH_PRIVATE_KEY"
-
-# ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" "$AWS_USER"@"$ZEELZ_EC2_IP" "echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin &&
-# docker pull $DOCKER_USER/express-gitlab &&
-# docker ps -aq | xargs docker rm -f || true &&
-# docker run -d -p 5500:5500 -e PORT=5500 -e DB_USER=zeelz -e DB_HOST=4.tcp.eu.ngrok.io -e DB_NAME=postgres -e DB_PORT=19684 zeelz/express-gitlab"
+ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" "$AWS_USER@$ZEELZ_EC2_IP" "echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin &&
+docker pull $DOCKER_USER/express-gitlab &&
+docker ps -aq | xargs docker rm -f || true &&
+docker run -d -p 5500:5500 -e PORT=5500 -e DB_USER=zeelz -e DB_HOST=4.tcp.eu.ngrok.io -e DB_NAME=postgres -e DB_PORT=19684 zeelz/express-gitlab"
