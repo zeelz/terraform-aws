c7e4604d2610fb5a6430246fad44c4b0a5bc9792
Keme <keme.kenneth@gmail.com>
2025-10-23 09:55:24 +0100

    Update db connection
    allow non-ssl

--- db.ts
+++ db.ts
@@ -8,5 +8,8 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT as unknown as number
+  port: process.env.DB_PORT as unknown as number,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
