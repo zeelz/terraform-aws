2b1ceb1c679ce6968de7f23054cd95bcdb984416
Keme <keme.kenneth@gmail.com>
2025-11-02 00:38:32 +0100

    Update terrform config
    Upgrade to t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,8 +55,8 @@ resource "aws_security_group" "devops_test_sg" {
 }
 
 resource "aws_instance" "zeelz_db_ec2" {
-    ami                             = "ami-08982f1c5bf93d976"
-    instance_type                   = "t2.micro"
+    ami                             = "ami-0bdd88bd06d16ba03" #"ami-08982f1c5bf93d976"
+    instance_type                   = "t3.micro" #upgraded from t2.micro 'cos of k8s
     # vpc_security_group_ids          = ["sg-033eebe707ffaa9c2"]
     vpc_security_group_ids          = [aws_security_group.devops_test_sg.id]
     key_name                        = aws_key_pair.zeelz_db.key_name
