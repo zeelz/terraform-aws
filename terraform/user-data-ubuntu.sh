47f858d473dbf257d52f8b51d88f4bfdcdbb9c6a
Keme <keme.kenneth@gmail.com>
2026-02-05 17:51:22 +0100

    Update terrform workflow
    Destroy resources

--- terraform/user-data-ubuntu.sh
+++ terraform/user-data-ubuntu.sh
@@ -44,6 +44,8 @@ sudo newgrp docker
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 
-minikube start
+# minikube start ## minikube won't start with docker driver as root
+sudo -u ubuntu minikube start --driver=docker
+
 
 alias kubectl='minikube kubectl --'
