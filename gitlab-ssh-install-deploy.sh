5b6333ad03e6effcfa99b72454ca619925449340
Keme <keme.kenneth@gmail.com>
2026-02-23 07:57:02 +0100

    Update pipeline to deploy container

--- gitlab-ssh-install-deploy.sh
+++ gitlab-ssh-install-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 apk add --no-cache openssh-client
 
@@ -10,4 +11,4 @@ chmod 400 "$SSH_PRIVATE_KEY" # stored as file
 ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" "$AWS_USER@$ZEELZ_EC2_IP" "echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin &&
 docker pull $DOCKER_USER/express-gitlab &&
 docker ps -aq | xargs docker rm -f || true &&
-docker run -d -p 5500:5500 -e PORT=5500 -e DB_USER="$DB_USER" -e DB_PASSWORD="$DB_PASSWORD" -e DB_HOST="$DB_HOST" -e DB_NAME="$DB_NAME" -e DB_PORT="$DB_PORT" zeelz/express-gitlab"
+docker run -d -p 5500:5500 -e PORT=5500 -e DB_USER="$DB_USER" -e DB_PASSWORD="$DB_PASSWORD" -e DB_HOST="$DB_HOST" -e DB_NAME="$DB_NAME" -e DB_PORT="$DB_PORT" $DOCKER_USER/express-gitlab"
